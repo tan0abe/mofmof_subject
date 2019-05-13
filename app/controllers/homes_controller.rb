a8c4ccf78b5952f124fb4a4aeaf637610cf90d58
ryota tanabe <teni0abe@gmail.com>
2019-05-13 16:37:24 +0900

    [update]homes_controllerの各アクションの完成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,10 +3,11 @@ class HomesController < ApplicationController
 
   def new
     @home = Home.new
+    @home.near_stations.build
   end
 
   def create
-    if Home.create
+    if Home.create(home_params)
       redirect_to homes_path
     else
       render 'new'
@@ -18,28 +19,38 @@ class HomesController < ApplicationController
   end
 
   def show
-    
+    @near_stations = @home.near_stations
   end
 
   def edit
-    
-  end
-
-  def confirm
-
+    @near_stations = @home.near_stations
   end
 
   def update
-    
+    if @home.update(update_params)
+      redirect_to homes_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-    
+    @home.destroy
+      redirect_to homes_path
   end
 
   private
 
-  def set_params
+  def set_home
     @home = Home.find(params[:id])
+  end
+
+  def home_params
+    params.require(:home).permit(:name, :price, :address, :year, :comment, near_stations_attributes: [:line, :station, :minute_walk])
+  end
+
+  def update_params
+    params.require(:home).permit(:name, :price, :address, :year, :comment, near_stations_attributes:
+                                [:line, :station, :minute_walk, :_destroy, :id])
   end
 end
