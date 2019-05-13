ae4e555c8a7e28b378310c087d87f0ba7825e708
ryota tanabe <teni0abe@gmail.com>
2019-05-13 15:18:23 +0900

    [update]homesコントローラを途中まで作成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,2 +1,45 @@
 class HomesController < ApplicationController
+  before_action :set_params, only: %i[show eidt update destroy]
+
+  def new
+    @home = Home.new
+  end
+
+  def create
+    if Home.create
+      redirect_to homes_path
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @homes = Home.all
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def confirm
+
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def set_params
+    @home = Home.find(params[:id])
+  end
 end
