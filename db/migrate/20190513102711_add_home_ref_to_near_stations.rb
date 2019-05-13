8102badb6a052380f860592162f52d4469097738
ryota tanabe <teni0abe@gmail.com>
2019-05-13 19:28:42 +0900

    [fix]カラム名の修正

--- db/migrate/20190513102711_add_home_ref_to_near_stations.rb
+++ db/migrate/20190513102711_add_home_ref_to_near_stations.rb
@@ -1,5 +1,5 @@
 class AddHomeRefToNearStations < ActiveRecord::Migration[5.0]
   def change
-    add_reference :near_stations, :Home, foreign_key: true
+    add_reference :near_stations, :home, foreign_key: true
   end
 end
