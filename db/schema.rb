ff7e712104d31d23455d5b305770f64b09b4354c
ryota tanabe <teni0abe@gmail.com>
2019-05-13 17:26:06 +0900

    [update]foreign_keyの設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190513025300) do
+ActiveRecord::Schema.define(version: 20190513081100) do
 
   create_table "homes", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20190513025300) do
     t.integer  "minute_walk"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "Home_id"
+    t.index ["Home_id"], name: "index_near_stations_on_Home_id"
   end
 
 end
